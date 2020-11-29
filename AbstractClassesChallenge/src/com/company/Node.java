ff0ad2f0bd137e3855f7d750252e03c6724de3d3
Oghenevwearhe Akemu <unsafemango@gmail.com>
2020-11-29 02:03:32 +0100

    I'm a bit confused here

---
+++ AbstractClassesChallenge/src/com/company/Node.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Node extends ListItem {
+    public Node(Object value) {
+        super(value);
+    }
+
+    @Override
+    ListItem next() {
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem setNext(ListItem item) {
+        this.rightLink = item; //saves item to this.rightLink
+        return this.rightLink;
+    }
+
+    @Override
+    ListItem previous() {
+        return this.leftLink;
+    }
+
+    @Override
+    ListItem setPrevious(ListItem item) {
+        this.leftLink = item;
+        return this.leftLink;
+    }
+
+    @Override
+    int compareTo(ListItem item) {
+        if (item != null) {
+            return ((String) super.getValue()).compareTo((String) item.getValue());
+            //String class has got the compare parameter built in so it makes it easier
+        } else {
+            return -1;
+        }
+    }
+}
